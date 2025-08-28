db8a64c0230a66411c6ea13317afe4c89c08d976
Amit Rajput <amitrajput98267313@gmail.com>
2025-08-28 17:03:21 +0530

    adding Router

--- Myntra-react-clone/src/main.jsx
+++ Myntra-react-clone/src/main.jsx
@@ -2,9 +2,25 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import {RouterProvider} from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { path: "/", element: <PostList />/* , loader: postLoader*/ },
+      {
+        path: "/bag",
+        element: <CreatePost />,
+        /*action: createPostAction,*/
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router ={router}></RouterProvider>
+  <React.StrictMode>,
 )
